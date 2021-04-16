99ffad15a910cf5aedd21efa5e20d6b925e663cb
osmankbk <mackiekbk@yahoo.com>
2021-04-16 17:06:34 -0400

    Deletes down arrow display in scroll event

--- js/scripts.js
+++ js/scripts.js
@@ -1,25 +1,21 @@
 //Get elements to style
 const firstNavBar = document.querySelector('.first');
 const secondNavBar = document.querySelector('.second');
-const ofSet = 
-console.log(secondNavBar);
+const downArrow = document.querySelector('.down');
+
+console.log(downArrow);
 
 window.addEventListener('scroll', () => {
     if(window.scrollY > 50) {
         console.log(window.scrollY);
         firstNavBar.style.display = "TranslateY(-100%)";
         secondNavBar.style.transform = "TranslateY(1%)";
+
     } else {
-        if(window.scrollY < 50 && window.scrollY > 0) {
+        if(window.scrollY < 50 && window.scrollY > -1) {
             firstNavBar.style.display = "TranslateY(1%)";
             secondNavBar.style.transform = "TranslateY(-100%)";
+
         }
     }
-    // else if(window.scrollY < 50 && window.scrollY > 0) {
-    //     firstNavBar.style.transform = "translateY(100%)"
-    //     secondNavBar.style.transform = "TranslateY(-100%)";
-    // }
-    console.log(window.scrollY);
-    // firstNavBar.style.transform = "translateY(-100%)";
-    // secondNavBar.style.transform = "TranslateY(1%)";
 });
