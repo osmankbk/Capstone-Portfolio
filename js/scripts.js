3b512290d86cdd921faf81254721756b73f663de
osmankbk <mackiekbk@yahoo.com>
2021-04-23 14:59:46 -0400

    Adds comments

--- js/scripts.js
+++ js/scripts.js
@@ -3,15 +3,16 @@ const firstNavBar = document.querySelector('.first');
 const secondNavBar = document.querySelector('.second');
 const downArrow = document.querySelector('.down');
 
-console.log(downArrow);
-
+//Scroll event
 window.addEventListener('scroll', () => {
+    //Display second header when condition met
     if(window.scrollY > 50) {
         console.log(window.scrollY);
         firstNavBar.style.display = "TranslateY(-100%)";
         secondNavBar.style.transform = "TranslateY(1%)";
 
     } else {
+        //Hide it otherwise
         if(window.scrollY < 50 && window.scrollY > -1) {
             firstNavBar.style.display = "TranslateY(1%)";
             secondNavBar.style.transform = "TranslateY(-100%)";
